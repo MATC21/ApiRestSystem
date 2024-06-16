f6ebadda544b3b12ef6e4ac7a351aedfc28314d5
MATC21 <ma.ona@itq.edu.ec>
2024-06-15 20:49:30 -0500

    Add project files.

---
+++ ApiRestSystem/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiRestSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiRestSystem.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options) { 
+
+        }
+        public DbSet<Productos> Productos { get; set; }
+    }
+}
---
+++ ApiRestSystem/Models/Productos.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiRestSystem.Models
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
